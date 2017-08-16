9fadc905e00b36a5c70f4cb98e15107be6ed4444
Donovan <dvnpete@gmail.com>
2017-08-16 12:03:57 -0700

    Voices of Lifers
    Version 2.0.1 - WP Theme

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * vol-wp Theme Customizer
+ *
+ * @package vol-wp
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function voicesoflifers_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'voicesoflifers_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'voicesoflifers_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'voicesoflifers_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function voicesoflifers_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function voicesoflifers_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function voicesoflifers_customize_preview_js() {
+	wp_enqueue_script( 'voicesoflifers-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'voicesoflifers_customize_preview_js' );
